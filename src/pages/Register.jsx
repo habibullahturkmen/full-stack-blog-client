5f5c4f673f7c0c64de9aa19d7dc3d732405288d0
habibullahturkmen <habibullah_turkmen@yahoo.com>
2022-12-09 10:24:08 +0430

    register page initial ui and login page ui updated

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,9 +1,18 @@
 import React from "react";
+import {Link} from "react-router-dom";
 
 const Register = () => {
     return (
-        <div>
-            Register
+        <div className="auth">
+            <h1>Register</h1>
+            <form>
+                <input required type="text" placeholder="username" />
+                <input required type="email" placeholder="email" />
+                <input required type="password" placeholder="password" />
+                <button type="submit">Register</button>
+                <p>This is an error!</p>
+                <span>Have an account? <Link to="/login">Login</Link></span>
+            </form>
         </div>
     );
 }
